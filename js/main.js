ed291cf0427c3f8011fdba33d2ae5ddfe45e6959
Constantine <qostyab@gmail.com>
2015-12-05 00:27:46 +0300

    slider css fixes

--- js/main.js
+++ js/main.js
@@ -62,6 +62,8 @@
                                     prevText: '',
                                     nextText: ''
                                 });
+
+
                             }
                         }
                     }
